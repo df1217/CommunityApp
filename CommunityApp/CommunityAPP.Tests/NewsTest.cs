3fc2511248e9d938661222e37d29c18ba9001ba3
df1217 <dffrank1217@gmail.com>
2017-02-02 20:51:15 -0800

    Lab 3 Product Version of the community website

---
+++ CommunityApp/CommunityAPP.Tests/NewsTest.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+using CommunityApp.Models;
+using System;
+
+namespace CommunityAPP.Test
+{
+    public class NewsTest
+    {
+        [Fact]
+        public void TestTitleGetter()
+        {
+            //Arrange
+            var n = new News();
+
+
+            //Act
+            n.Title = "Yee haw";
+
+            //Assert
+            Assert.Equal("Yee haw", n.Title);
+
+
+        }
+        [Fact]
+        public void TestNewsDateConstructor()
+        {
+            //Arrange
+            var n = new News();
+
+
+            //Act
+            n.Date = new DateTime(2017, 1, 17);
+
+
+            //Assert
+            Assert.True(n.Date.Day == 17 &&
+                n.Date.Year == 2017
+                && n.Date.Month == 1);
+
+
+        }
+
+        [Fact]
+        public void TestNewsStoryGetter()
+        {
+            //Arrange
+            var n = new News();
+            
+            //Act
+            n.Story = "This is a test";
+
+            //Assert
+            Assert.Equal("This is a test", n.Story );
+
+        }
+
+        [Fact]
+        public void TestNewsStorySetter()
+        {
+            //Arrange
+            var n = new News();
+
+            //Act
+            n.Story = "This is a test";
+            n.Story = "This is a test 2";
+
+            //Assert
+            Assert.NotEqual("This is a test", n.Story);
+            Assert.Equal("This is a test 2", n.Story);
+
+        }
+
+    }
+}
---
+++ CommunityApp/src/CommunityApp/Models/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityApp.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Story { get; set; }
+
+    }
+}
